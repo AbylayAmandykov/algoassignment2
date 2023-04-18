e30743fb044ae670491ed9d112b2a906fa6d151d
Abylay <abylay0505@gmail.com>
2023-04-18 14:39:53 +0600

    first method added

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -6,5 +6,12 @@ public class MyArrayList<T> {
         this.arr = (T[]) new Object[5];
         this.size = 0;
     }
+    public void increaseBuffer() {
+        T[] newArr = (T[]) new Object[arr.length+5];
+        for(int i = 0; i< arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        arr = newArr;
+    }
 
 }
