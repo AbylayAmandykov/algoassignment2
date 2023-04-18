c3f40ac4a6e4f2593ff16bcd2fb9f27e508d3c52
Abylay <abylay0505@gmail.com>
2023-04-18 15:59:46 +0600

    methods added

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,4 +1,4 @@
-public class MyArrayList<T> {
+public class MyArrayList<T> implements MyList {
     private T[] arr;
     private int size = 0;
 
@@ -6,41 +6,94 @@ public class MyArrayList<T> {
         this.arr = (T[]) new Object[5];
         this.size = 0;
     }
+
     public void increaseBuffer() {
-        T[] newArr = (T[]) new Object[arr.length+5];
-        for(int i = 0; i< arr.length; i++) {
+        T[] newArr = (T[]) new Object[arr.length + 5];
+        for (int i = 0; i < arr.length; i++) {
             newArr[i] = arr[i];
         }
         arr = newArr;
     }
+
     public T getElement(int index) {
         checkIndex(index);
         return arr[index];
     }
+
     public int getSize() {
         return size;
     }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object item) {
+        if(size == arr.length) {
+            increaseBuffer();
+        }
+        arr[size++] = (T) item;
+    }
+
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
     public void clear() {
         this.arr = (T[]) new Object[5];
         this.size = 0;
     }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
     public void delete(int index) {
         checkIndex(index);
-        for(int i = index + 1; i<size; i++) {
-            arr[i-1] = arr[i];
+        for (int i = index + 1; i < size; i++) {
+            arr[i - 1] = arr[i];
         }
         size--;
     }
-    public void add(T element) {
-        if(size == arr.length) {
-            increaseBuffer();
-        }
-        arr[size++] = element;
-    }
-    public void checkIndex(int index){
-        if(index < 0 || index>=size){
+    public void checkIndex(int index) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
 
-
+    }
 }
