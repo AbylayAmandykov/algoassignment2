a57f50b127940ad3b691bf72b097b54af0284c28
Abylay <abylay0505@gmail.com>
2023-04-20 15:31:22 +0600

    methods added

--- src/Main.java
+++ src/Main.java
@@ -14,15 +14,17 @@ public class Main  {
 //
 //        System.out.println(arrayList);
 //        arrayList.sort();
-
+        System.out.println(linkedList.contains(5));
         linkedList.add(sc.nextInt());
         linkedList.add(sc.nextInt());
-        linkedList.add(sc.nextInt());
-        linkedList.add(3, 2);
-        linkedList.add(sc.nextInt());
+//        linkedList.add(sc.nextInt());
+//        linkedList.add(sc.nextInt());
+//        linkedList.add(3, 2);
+//        linkedList.add(sc.nextInt());
          //linkedList.add(sc.nextInt(), sc.nextInt());
         System.out.println(linkedList.size());
         System.out.println(linkedList);
+        System.out.println(linkedList.contains(5));
 
 
 
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -23,6 +23,13 @@ public class MyLinkedList<E> implements MyList {
 
     @Override
     public boolean contains(Object o) {
+        Node<E> current = head;
+        while (current != null) {
+            if (current.value.equals(o)) {
+                return true;
+            }
+            current = current.next;
+        }
         return false;
     }
 
@@ -37,7 +44,6 @@ public class MyLinkedList<E> implements MyList {
         tail = newNode;
         size++;
     }
-
     @Override
     public void add(Object item, int index) {
         checkIndex(index);
@@ -67,14 +73,61 @@ public class MyLinkedList<E> implements MyList {
             size++;
         }
     }
-    @Override
     public boolean remove(Object item) {
+        Node currentNode = head;
+        while (currentNode != null && !currentNode.value.equals(item)) {
+            currentNode = currentNode.next;
+        }
+        if (currentNode != null) {
+            if (currentNode == head) {
+                head = currentNode.next;
+                if (head != null) {
+                    head.previous = null;
+                }
+            } else if (currentNode == tail) {
+                tail = currentNode.previous;
+                if (tail != null) {
+                    tail.next = null;
+                }
+            } else {
+                currentNode.previous.next = currentNode.next;
+                currentNode.next.previous = currentNode.previous;
+            }
+            size--;
+            return true;
+        }
         return false;
     }
 
     @Override
     public Object remove(int index) {
-        return null;
+        checkIndex(index);
+        Node curNode;
+        if (index < size / 2) {
+            curNode = head;
+            for (int i = 0; i < index; i++) {
+                curNode = curNode.next;
+            }
+        } else {
+            curNode = tail;
+            for (int i = size - 1; i > index; i--) {
+                curNode = curNode.previous;
+            }
+        }
+        Node prevNode = curNode.previous;
+        Node nextNode = curNode.next;
+        if (prevNode != null) {
+            prevNode.next = nextNode;
+        } else {
+            head = nextNode;
+        }
+        if (nextNode != null) {
+            nextNode.previous = prevNode;
+        } else {
+            tail = prevNode;
+        }
+        size--;
+        return curNode.value;
     }
 
     @Override
@@ -86,22 +139,72 @@ public class MyLinkedList<E> implements MyList {
 
     @Override
     public Object get(int index) {
-        return null;
+        checkIndex(index);
+        Node currentNode;
+        if (index < size / 2) {
+            currentNode = head;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+        } else {
+            currentNode = tail;
+            for (int i = size - 1; i > index; i--) {
+                currentNode = currentNode.previous;
+            }
+        }
+        return currentNode.value;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        Node curNode = head;
+        int index = 0;
+        while (curNode != null) {
+            if (curNode.value.equals(o)) {
+                return index;
+            }
+            curNode = curNode.next;
+            index++;
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        Node curNode = tail;
+        int index = size - 1;
+        while (curNode != null) {
+            if (curNode.value.equals(o)) {
+                return index;
+            }
+            curNode = curNode.previous;
+            index--;
+        }
+        return -1;
     }
-
     @Override
     public void sort() {
+        if (size < 2) {
+            return;
+        }
+        Node current = head;
+        while (current != null) {
+            Node min = current;
+            Node innerCurrent = current.next;
+            while (innerCurrent != null) {
+                if (((Comparable) innerCurrent.value).compareTo(min.value) < 0) {
+                    min = innerCurrent;
+                }
+                innerCurrent = innerCurrent.next;
+            }
 
+            if (min != current) {
+                Object temp = current.value;
+                current.value = min.value;
+                min.value = temp;
+            }
+            current = current.next;
+        }
     }
     private void checkIndex(int index){
         if(index < 0 || index>=size){
