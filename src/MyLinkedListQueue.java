c80229f15b7de8c25cd50bb491b99ea611ae4b53
Abylay <abylay0505@gmail.com>
2023-04-30 23:48:10 +0600

    docs added

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -3,23 +3,44 @@ public class MyLinkedListQueue<T> {
     public MyLinkedListQueue() {
         front = new MyLinkedList<>();
     }
+    /*
+    adds an element to the back of the queue using the add method for MyLinkedList
+    @element - added element
+    */
     public void enqueue(T element) {
         front.add(element);
     }
+    /*
+    removes and returns the front element of the queue using the remove method
+    @throws NoSuchFieldException if queue is empty
+    */
     public T dequeue() throws NoSuchFieldException {
         isEmptyThrowException();
-        return (T) front.remove(0);
+        return (T) front.remove(0); // returning the removed element
     }
+    /*
+    returns the front element of the queue without removing it using the get method
+    @throws NoSuchFieldException if queue is empty
+    */
     public T peek() throws NoSuchFieldException {
         isEmptyThrowException();
         return (T) front.get(0);
     }
+    /*
+    returns true if the queue is empty or false if it is not using the isEmpty method
+    */
     public boolean isEmpty() {
         return front.size() == 0;
     }
+    /*
+    returns the number of elements in the queue using the size method
+    */
     public int size() {
         return front.size();
     }
+    /*
+    exception by using isEmpty method
+    */
     private void isEmptyThrowException() throws NoSuchFieldException {
         if (isEmpty()) {
             throw new NoSuchFieldException();
