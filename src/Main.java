8fe4a6c8ace4657f358a346133a5c93a36d2de49
Abylay <abylay0505@gmail.com>
2023-04-30 15:52:25 +0600

    stack for linkedlist added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MyArrayListStack<Integer> arrayListStack = new MyArrayListStack();
-        System.out.println("Stack for ArrayList");
+        System.out.println("Stack for ArrayList:");
         System.out.println("Is empty? " + arrayListStack.isEmpty());
         arrayListStack.push(5);
         arrayListStack.push(3);
@@ -12,6 +12,17 @@ public class Main {
         System.out.println("Size: " + arrayListStack.size());
         System.out.println("Removed element: " + arrayListStack.pop());
         System.out.println("Top element: " + arrayListStack.peek());
+        System.out.println();
+        MyLinkedListStack<Integer> linkedListStack = new MyLinkedListStack();
+        System.out.println("Stack for LinkedList:");
+        System.out.println("Is empty? " + linkedListStack.isEmpty());
+        linkedListStack.push(7);
+        linkedListStack.push(6);
+        linkedListStack.push(14);
+        System.out.println("Is empty? " + linkedListStack.isEmpty());
+        System.out.println("Size: " + linkedListStack.size());
+        System.out.println("Removed element: " + linkedListStack.pop());
+        System.out.println("Top element: " + linkedListStack.peek());
 
     }
 }
--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,2 +1,29 @@
-public class MyLinkedListStack {
+import java.util.EmptyStackException;
+public class MyLinkedListStack<T> {
+    private MyLinkedList<T> top;
+    public MyLinkedListStack() {
+        top = new MyLinkedList<>();
+    }
+    public void push(T element) {
+        top.add(element);
+    }
+    public T pop() {
+        isEmptyThrowException();
+        return (T) top.remove(top.size()-1);
+    }
+    public T peek() {
+        isEmptyThrowException();
+        return (T) top.get(top.size()-1);
+    }
+    public boolean isEmpty() {
+        return top.size() == 0;
+    }
+    public int size() {
+        return top.size();
+    }
+    public void isEmptyThrowException() {
+        if(isEmpty()) {
+            throw new EmptyStackException();
+        }
+    }
 }
