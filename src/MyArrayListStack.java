6b11b72c5e5d65a512b219f86108c5cb5338d956
Abylay <abylay0505@gmail.com>
2023-04-30 14:39:07 +0600

    new method added

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,7 +1,14 @@
 public class MyArrayListStack<T> {
     MyArrayList arrayList = new MyArrayList();
+    private MyArrayList<T> arr;
+    public MyArrayListStack() {
+        arr = new MyArrayList<>();
+    }
     public void push(T element) {
         arrayList.add(element);
+    }
+    public boolean isEmpty() {
+        return arr.size() == 0;
     }
 
 }
