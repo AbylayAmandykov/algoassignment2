15a9ab4f8594512a05d1680f466953fdd2eda7ee
Abylay <abylay0505@gmail.com>
2023-04-30 21:28:36 +0600

    method added

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,2 +1,9 @@
-public class MyArrayListQueue {
+public class MyArrayListQueue<T> {
+    private MyArrayList<T> arr;
+    public MyArrayListQueue() {
+        arr = new MyArrayList<>();
+    }
+    public void enqueue(T element) {
+        arr.add(element);
+    }
 }
