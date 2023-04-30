534913c498fdb64d60ac44d563355faf5425dcb7
Abylay <abylay0505@gmail.com>
2023-04-30 22:28:36 +0600

    method added

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,2 +1,10 @@
-public class MyLinkedListQueue {
+public class MyLinkedListQueue<T> {
+    private MyLinkedList<T> front;
+    public MyLinkedListQueue() {
+        front = new MyLinkedList<>();
+    }
+    public void enqueue(T element) {
+        front.add(element);
+    }
+
 }
