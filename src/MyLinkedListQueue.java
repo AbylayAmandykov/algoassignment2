51bcb06183075ae8a80a67df2bb82cd5f6b1a756
Abylay <abylay0505@gmail.com>
2023-04-30 22:37:48 +0600

    method added

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,18 +1,14 @@
 public class MyLinkedListQueue<T> {
     private MyLinkedList<T> front;
-
     public MyLinkedListQueue() {
         front = new MyLinkedList<>();
     }
-
     public void enqueue(T element) {
         front.add(element);
     }
-
     public boolean isEmpty() {
         return front.size() == 0;
     }
-
     private void isEmptyThrowException() throws NoSuchFieldException {
         if (isEmpty()) {
             throw new NoSuchFieldException();
